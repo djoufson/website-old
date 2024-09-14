ef37d4dd8c99d574be11a29701ba87ca2948df51
Djoufson CHE BENE <djouflegran@gmail.com>
2024-09-14 22:02:24 +0100

    style: implement the hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,48 +13,36 @@ module.exports = {
     extend: {
       colors: {
         primary: {
-          DEFAULT: '#0A855F',
-          dark: '#0A855F',
-          accentuation: '#0AA072',
+          DEFAULT: '#139AD7',
+          hover: '#0E7BCC',
         },
         secondary: {
-          DEFAULT: '#512BD4',
-          dark: '#3B12C8',
-          accentuation: '#7352E7',
+          DEFAULT: '#BEB932',
+          hover: '#A7A528',
         },
-        tertiary: {
-          DEFAULT: '#EBC006',
-          dark: '#B79918',
-          accentuation: '#F9D32C',
+        white: {
+          DEFAULT: '#F9FAFB',
+          hover: '#F3F4F6',
         },
+        black: {
+          DEFAULT: '#2D2D2D',
+          hover: '#3D3D3D',
+        },
+        selection: '#BEB932',
       },
       fontFamily: {
         special: ['Space Grotesk'],
         heading: ['Inter, sans-serif'],
       },
-      fontSize: {
-        'heading-1': '2.25rem', // 36px
-        'heading-2': '1.875rem', // 30px
-        'heading-3': '1.5rem', // 24px
-        'heading-4': '1.25rem', // 20px
-        'heading-5': '1rem', // 16px
-        'heading-6': '0.875rem', // 14px
-      },
       lineHeight: {
         'heading-1': '1.2',
         'heading-2': '1.25',
-        'heading-3': '1.3',
-        'heading-4': '1.35',
-        'heading-5': '1.4',
-        'heading-6': '1.5',
+        'heading-3': '1.3'
       },
       margin: {
         'heading-1': '1rem', // 16px
         'heading-2': '0.875rem', // 14px
         'heading-3': '0.75rem', // 12px
-        'heading-4': '0.625rem', // 10px
-        'heading-5': '0.5rem', // 8px
-        'heading-6': '0.375rem', // 6px
       },
     },
   },
